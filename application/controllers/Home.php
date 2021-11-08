db6380c9a547f049522a2efbfdf76ad8081abeaa
Rival <rifall.osd26@gmail.com>
2021-11-08 22:47:35 +0700

    ubah form vald lang, buat daftar

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -18,10 +18,60 @@ class Home extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+	public function __construct(){
+		parent::__construct();
+        $this->load->model('m_daftar');
+		// if($this->session->login == '') redirect();
+	}
+	 
 	public function index()
 	{
 		$this->load->view('templates/header');
 		$this->load->view('index');
 		$this->load->view('templates/footer');
+	}
+
+	public function login()
+	{
+		$this->m_login->login();
+	}
+
+	public function daftar()
+	{
+		// $this->m_daftar->daftar();
+		$nama = $this->input->post('nama');
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+        $foto = $_FILES['foto'];
+
+        $this->form_validation->set_rules('nama', 'Nama', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[users.email]');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        if($this->form_validation->run() == FALSE){
+            $this->index();
+        }else{
+            
+            if(!$foto == ''){
+                $config['upload_path'] = './assets/img';
+                $config['allowed_types'] = 'jpg|jpeg|png';
+    
+                $this->load->library('upload', $config);
+                if($this->upload->do_upload('foto')){
+                    $foto = $this->upload->data('file_name');
+                }else{
+                    $this->index();
+                }
+            }else{};
+    
+            $data = array(
+                'nama' => html_escape($nama),
+                'email' => html_escape($email),
+                'password' => html_escape($password),
+                'foto' => $foto,
+                'is_active' => 0
+            );
+            $this->db->insert('users', $data);
+            $this->index();
+        }
 	}
 }
--- application/views/index.php
+++ application/views/index.php
@@ -1,20 +1,25 @@
 <div class="wave"></div>
 <div class="container-fluid row align-content-center vh-100 mx-auto">
+	<?php if(validation_errors()): ?>
+	<div class="w-100 alert alert-danger text-center" role="alert">  
+		<?php echo validation_errors(); ?>
+	</div>
+	<?php endif;?>
     <div class="container p-2 border border-primary mx-auto shadow bg-light rounded">
         <ul class="nav nav-pills mb-3 justify-content-around" id="pills-tab" role="tablist">
             <li class="nav-item">
                 <a class="nav-link aktif" id="pills-login-tab" data-toggle="tab" href="#pills-login" role="tab"
-                    aria-controls="pills-login" aria-selected="true">Login</a>
+                    aria-controls="pills-login" aria-selected="true"><b>Login</b></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" id="pills-daftar-tab" data-toggle="pill" href="#pills-daftar" role="tab"
-                    aria-controls="pills-daftar" aria-selected="false">Daftar</a>
+                    aria-controls="pills-daftar" aria-selected="false"><b>Daftar</b></a>
             </li>
         </ul>
         <hr>
         <div class="tab-content" id="pills-tabContent">
             <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="pills-login-tab">
-                <form class="text-center" method="POST" action="http://localhost/rvl/login/proses_login">
+                <form class="text-center" method="POST" action="<?= base_url('login');?>">
                     <div class="col mt-5">
                         <div class="form-group">
                             <input type="text" id="email" placeholder="Masukkan Email" autocomplete="off" required
@@ -33,34 +38,29 @@
             <div class="tab-pane fade" id="pills-daftar" role="tabpanel" aria-labelledby="pills-daftar-tab">
                 <div class="tab-pane fade show active" id="pills-daftar" role="tabpanel"
                     aria-labelledby="pills-daftar-tab">
-                    <form class="text-center" method="POST" action="http://localhost/rvl/login/proses_daftar">
-                        <div class="col">
+                    <form class="text-center" method="POST" enctype="multipart/form-data" action="<?= base_url('home/daftar');?>" accept-charset="utf-8">
+                        <div class="col form-group">
                             <figure class="figure">
                                 <img src="<?= base_url('assets/img/user.png');?>"
-                                    class="figure-img shadow w-50 rounded-circle img-fluid" alt="Foto" width="150"
-                                    height="125">
+                                    class="figure-img shadow rounded-circle size-foto" alt="Foto">
                             </figure>
                             <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="foto" name="foto"
-                                    onchange="gantiValueLabel(this)" required>
-                                <label class="custom-file-label " for="foto">Pilih foto...</label>
+                                <input type="file" class="custom-file-input" id="foto" name="foto" onchange="gantiValueLabel(this)" required>
+                                <label class="custom-file-label" for="foto">Pilih foto...</label>
                             </div>
                         </div>
                         <div class="col mt-5">
                             <div class="form-group">
-                                <input type="text" id="nama" placeholder="Masukkan Nama" autocomplete="off" required
-                                    name="nama" class="input--custom mt-1 pl-2">
+                                <input type="text" placeholder="Masukkan Nama" value="<?php echo set_value('nama'); ?>" required name="nama" class="input--custom mt-1 pl-2">
                             </div>
                             <div class="form-group">
-                                <input type="text" id="email" placeholder="Masukkan Email" autocomplete="off" required
-                                    name="email" class="input--custom mt-1 pl-2">
+                                <input type="text" placeholder="Masukkan Email" required name="email" class="input--custom mt-1 pl-2">
                             </div>
                             <div class="form-group">
-                                <input type="password" id="password" placeholder="Masukkan Password" required
-                                    name="password" class="input--custom mt-2 pl-2">
+                                <input type="password" placeholder="Masukkan Password" required name="password" class="input--custom mt-2 pl-2">
                             </div>
-                            <button type="submit" class="btn btn-primary mb-2 mt-3">Daftar</button>
                         </div>
+						<button type="submit" class="btn btn-primary mb-2 mt-3">Daftar</button>
                     </form>
                 </div>
             </div>
