67173e138fd7856c1700a5618fd1b0cbbc7f48ed
Rival <rifall.osd26@gmail.com>
2021-11-08 13:58:36 +0700

    init

---
+++ application/language/english/form_validation_lang.php
@@ -0,0 +1,34 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['form_validation_required']		= 'Kolom "{field}" wajib diisi.';
+$lang['form_validation_isset']			= 'Kolom "{field}" harus mempunyai nilai.';
+$lang['form_validation_valid_email']		= 'Kolom "{field}" tidak valid.';
+$lang['form_validation_valid_emails']		= 'Kolom "{field}" harus berisi seluruh email yang valid.';
+$lang['form_validation_valid_url']		= 'Kolom "{field}" harus berisi URL yang valid..';
+$lang['form_validation_valid_ip']		= 'Kolom "{field}" harus berisi IP yang valid..';
+$lang['form_validation_valid_base64']		= 'Kolom "{field}" harus berisi string Base64 yang valid.';
+$lang['form_validation_min_length']		= 'Panjang "{field}" setidaknya paling sedikit "{param}" karakter.';
+$lang['form_validation_max_length']		= 'Panjang "{field}" tidak boleh lebih dari "{param}" karakter';
+$lang['form_validation_exact_length']		= 'Panjang "{field}" harus tepat "{param}" karakter.';
+$lang['form_validation_alpha']			= 'Kolom "{field}" hanya boleh berisi alfabet.';
+$lang['form_validation_alpha_numeric']		= 'Kolom "{field}" hanya boleh berisi angka dan alfabet.';
+$lang['form_validation_alpha_numeric_spaces']	= 'Kolom "{field}" hanya boleh berisi angka,alfabet and spasi.';
+$lang['form_validation_alpha_dash']		= 'The "{field}" hanya boleh berisi angka,alfabet, garis bawah _, dan strip -.';
+$lang['form_validation_numeric']		= 'Kolom "{field}" hanya boleh berisi angka.';
+$lang['form_validation_is_numeric']		= 'Kolom "{field}" hanya boleh berisi angka.';
+$lang['form_validation_integer']		= 'Kolom "{field}" hanya boleh berisi bilangan bulat.';
+$lang['form_validation_regex_match']		= 'Kolom "{field}" tidak sesuai format.';
+$lang['form_validation_matches']		= 'Kolom "{field}" tidak sama dengan kolom "{param}".';
+$lang['form_validation_differs']		= 'Kolom "{field}" harus berbeda dengan kolom "{param}".';
+$lang['form_validation_is_unique'] 		= 'Kolom "{field}" harus berbeda dengan yang sudah ada.';
+$lang['form_validation_is_natural']		= 'Kolom "{field}" harus berisi bilangan positif.';
+$lang['form_validation_is_natural_no_zero']	= 'Kolom "{field}" harus berisi bilangan positif dan lebih besar dari 0.';
+$lang['form_validation_decimal']		= 'Kolom "{field}" harus berisi bilangan desimal.';
+$lang['form_validation_less_than']		= 'Kolom "{field}" harus kurang dari "{param}".';
+$lang['form_validation_less_than_equal_to']	= 'Kolom "{field}" harus kurang dari atau sama dengan "{param}".';
+$lang['form_validation_greater_than']		= 'Kolom "{field}" harus lebih dari "{param}".';
+$lang['form_validation_greater_than_equal_to']	= 'Kolom "{field}" harus lebih dari atau sama dengan "{param}".';
+$lang['form_validation_error_message_not_set']	= 'Tidak bisa mengakses pesan error dari kolom "{field}".';
+$lang['form_validation_in_list']		= 'Kolom "{field}" harus ada di: "{param}".';
