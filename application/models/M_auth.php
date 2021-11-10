8ff9667df6faf3bc5ddfe1a34707952afcfd3ce8
Rival <rifall.osd26@gmail.com>
2021-11-10 15:04:41 +0700

    ganti m_daftar jadi m_auth

--- application/models/M_auth.php
+++ application/models/M_auth.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class M_daftar extends CI_Model {
+class M_auth extends CI_Model {
 
 	public function kirimEmail($type,$email)
     {
